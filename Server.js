bffa19ccd86aae77b56b0b9a288f2999925e4547
tamilselvan8428 <154496243+tamilselvan8428@users.noreply.github.com>
2025-11-19 20:14:12 +0530

    Update CORS settings to allow all origins

--- Server.js
+++ Server.js
@@ -13,8 +13,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: process.env.FRONTEND_ORIGIN,
-  credentials: true
+  origin: "*",  
+  credentials: false 
 }));
 app.use(
   session({
